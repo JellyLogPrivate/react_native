d7b273737ad637f390b4db63ef03c3fb80170a23
parkhj2009 <eee0110010100@gmail.com>
2026-04-09 17:48:18 +0900

    Add SVG support (react-native-svg + transformer)

--- constants/theme.ts
+++ constants/theme.ts
@@ -7,47 +7,56 @@ import { Platform } from 'react-native';
 
 const tintColorLight = '#0a7ea4';
 const tintColorDark = '#fff';
+const main1Color = '#FFFCF5';
+const main2Color = '#FDFDFD';
+const sub1Color = '#B39C8D';
+const sub2Color = '#7B7876';
+const sub2_2Color = '#E4E3E3';
+const growthColor = '#B0F398';
+const heart1Color = '#FFCED0';
+const heart2Color = '#F3989C';
 
 export const Colors = {
-  light: {
-    text: '#11181C',
-    background: '#fff',
-    tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
-    tabIconSelected: tintColorLight,
-  },
-  dark: {
-    text: '#ECEDEE',
-    background: '#151718',
-    tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
-    tabIconSelected: tintColorDark,
-  },
+    light: {
+        text: '#11181C',
+        background: '#fff',
+        tint: tintColorLight,
+        icon: '#687076',
+        tabIconDefault: '#687076',
+        tabIconSelected: tintColorLight,
+    },
+    dark: {
+        text: '#ECEDEE',
+        background: '#151718',
+        tint: tintColorDark,
+        icon: '#9BA1A6',
+        tabIconDefault: '#9BA1A6',
+        tabIconSelected: tintColorDark,
+    },
 };
 
 export const Fonts = Platform.select({
-  ios: {
-    /** iOS `UIFontDescriptorSystemDesignDefault` */
-    sans: 'system-ui',
-    /** iOS `UIFontDescriptorSystemDesignSerif` */
-    serif: 'ui-serif',
-    /** iOS `UIFontDescriptorSystemDesignRounded` */
-    rounded: 'ui-rounded',
-    /** iOS `UIFontDescriptorSystemDesignMonospaced` */
-    mono: 'ui-monospace',
-  },
-  default: {
-    sans: 'normal',
-    serif: 'serif',
-    rounded: 'normal',
-    mono: 'monospace',
-  },
-  web: {
-    sans: "system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif",
-    serif: "Georgia, 'Times New Roman', serif",
-    rounded: "'SF Pro Rounded', 'Hiragino Maru Gothic ProN', Meiryo, 'MS PGothic', sans-serif",
-    mono: "SFMono-Regular, Menlo, Monaco, Consolas, 'Liberation Mono', 'Courier New', monospace",
-  },
+    ios: {
+        /** iOS `UIFontDescriptorSystemDesignDefault` */
+        sans: 'system-ui',
+        /** iOS `UIFontDescriptorSystemDesignSerif` */
+        serif: 'ui-serif',
+        /** iOS `UIFontDescriptorSystemDesignRounded` */
+        rounded: 'ui-rounded',
+        /** iOS `UIFontDescriptorSystemDesignMonospaced` */
+        mono: 'ui-monospace',
+    },
+    default: {
+        sans: 'normal',
+        serif: 'serif',
+        rounded: 'normal',
+        mono: 'monospace',
+    },
+    web: {
+        sans: "system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif",
+        serif: "Georgia, 'Times New Roman', serif",
+        rounded:
+            "'SF Pro Rounded', 'Hiragino Maru Gothic ProN', Meiryo, 'MS PGothic', sans-serif",
+        mono: "SFMono-Regular, Menlo, Monaco, Consolas, 'Liberation Mono', 'Courier New', monospace",
+    },
 });
