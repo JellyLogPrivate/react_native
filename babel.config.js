d00ebe4b89d09224be636839a87cd0eedcff2ecd
parkhj2009 <eee0110010100@gmail.com>
2026-04-09 18:03:45 +0900

    Add required Babel plugins for expo-router and react-native-reanimated.

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,12 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      // Required for expo-router
+      'expo-router/babel',
+      // Must be LAST
+      'react-native-reanimated/plugin',
+    ],
   };
 };
 
